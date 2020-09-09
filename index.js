f072382cfbc7d3178ee2c4c6414471383ba8d11c
Jonathan Bleibdrey <jonathanbleibdrey@Jonathans-MacBook-Air-2.local>
2020-09-09 18:00:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.slice(0,1).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-\s']+/g, '');
+  }
+
+  static titleize(string) {
+    let badboys = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let array = string.split(" ")
+    let capss = array.map(word => {
+      if (!badboys.includes(word)) {
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    })
+    capss[0] = this.capitalize(capss[0])
+    return capss.join(" ")
+  }
+
 }
